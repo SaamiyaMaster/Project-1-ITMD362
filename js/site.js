7f697f56c9905cfa177581860e4c541a246e86af
SaamiyaMaster <123525540+SaamiyaMaster@users.noreply.github.com>
2024-03-22 15:53:21 -0500

    fixing indentations

--- js/site.js
+++ js/site.js
@@ -4,16 +4,16 @@ jQuery(function($) {
   $('html').addClass('hasjs');
 });
   
-  function validateForm(){
-    var email = document.forms["SMForm"]["input-email"].value;
-	var password = document.forms["SMForm"]["input-password"].value;
+function validateForm(){
+  var email = document.forms["SMForm"]["input-email"].value;
+  var password = document.forms["SMForm"]["input-password"].value;
 	
-	if (password == "!") {
-	  alert("Password can not have ! in it, Change please");
-	  return false;
-	}
-	else {
-	  alert("Please check your Email inbox for verification!");
-	  return true;
-	}
+  if (password == "!") {
+	alert("Password can not have ! in it, Change please");
+	return false;
   }
+  else {
+	alert("Please check your Email inbox for verification!");
+	return true;
+  }
+}
