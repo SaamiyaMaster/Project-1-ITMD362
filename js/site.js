ee11452ee8f04357384564d8948090c8bc7fc86b
SaamiyaMaster <123525540+SaamiyaMaster@users.noreply.github.com>
2024-03-22 15:51:44 -0500

    added valication

--- js/site.js
+++ js/site.js
@@ -2,4 +2,17 @@
 jQuery(function($) {
   $('html').removeClass('nojs');
   $('html').addClass('hasjs');
-});ite
+});
+  
+  function validateForm(){
+    var email = document.forms["SMForm"]["input-email"].value;
+	var password = document.forms["SMForm"]["input-password"].value;
+	
+	if (password == "!") {
+	  alert("Password can not have ! in it, Change please");
+	  return false;
+	}
+	else {
+	  alert("Please check your Email inbox for verification!");
+	  return true;
+	}
